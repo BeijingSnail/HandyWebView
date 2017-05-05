22105907a5d4a4132b1002f2944310ff09d779a5
zhanghzhiqiang <807374599@qq.com>
2017-05-05 10:29:17 +0800

    add ProgressWebView

--- app/src/main/java/com/example/admin/handywebview/MainActivity.java
+++ app/src/main/java/com/example/admin/handywebview/MainActivity.java
@@ -1,9 +1,10 @@
 package com.example.admin.handywebview;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.handywebviewlibrary.CoreWebView;
+import com.example.handywebviewlibrary.ProgressWebView;
 
 public class MainActivity extends AppCompatActivity {
     private final String url = "https://huayiquan.com.cn/index.php?s=/home/article/content2/user_mobile/15142555427/aid/858/mobile/17774934497.html";
@@ -13,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CoreWebView coreWebView = (CoreWebView) findViewById(R.id.core_web_view);
+        ProgressWebView coreWebView = (ProgressWebView) findViewById(R.id.core_web_view);
 
         coreWebView.loadUrl(url);
 
