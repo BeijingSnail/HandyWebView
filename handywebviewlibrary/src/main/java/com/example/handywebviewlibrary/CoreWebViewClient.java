f73996902bc400382c44cd906111e1ac0f0a3c52
BeijingSnail <m15142555427@163.com>
2017-05-06 16:38:07 +0800

    add errorpage icon

--- handywebviewlibrary/src/main/java/com/example/handywebviewlibrary/CoreWebViewClient.java
+++ handywebviewlibrary/src/main/java/com/example/handywebviewlibrary/CoreWebViewClient.java
@@ -1,7 +1,8 @@
 package com.example.handywebviewlibrary;
 
 import android.annotation.TargetApi;
-import android.util.Log;
+import android.support.annotation.LayoutRes;
+import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewStub;
 import android.webkit.WebResourceError;
@@ -9,7 +10,9 @@ import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
+import static android.view.View.GONE;
 import static android.view.View.VISIBLE;
+import static com.example.handywebviewlibrary.R.id.reload;
 
 /**
  * Created by zhangzhiqiang on 2017/5/5.
@@ -19,36 +22,13 @@ public class CoreWebViewClient extends WebViewClient {
 
     private boolean isError = false;
     private ViewStub errorViewStub;
-
     private int errorLayout = R.layout.error_layout;
 
     @SuppressWarnings("deprecation")
     @Override
     public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
         super.onReceivedError(view, errorCode, description, failingUrl);
-        Log.e("zzq", "onReceivedError");
-
-        if (errorViewStub == null) {
-            errorViewStub = new ViewStub(view.getContext());
-//            errorViewStub.setInflatedId(errorLayout);
-            errorViewStub.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-//            errorViewStub.setLayoutInflater(LayoutInflater.from(webView.getContext()));
-            errorViewStub.setLayoutResource(errorLayout);
-            errorViewStub.setClickable(true);
-            view.addView(errorViewStub);
-
-//            View errorView = mErrorViewStub.inflate();
-//            errorView.findViewById(R.id.error_layout).setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    webView.reload();
-//                }
-//            });
-        } else {
-            errorViewStub.setVisibility(VISIBLE);
-        }
-
-//        showErrorPage(view);
+        showErrorPage(view);
     }
 
     @TargetApi(android.os.Build.VERSION_CODES.M)
@@ -56,62 +36,45 @@ public class CoreWebViewClient extends WebViewClient {
     public void onReceivedError(WebView view, WebResourceRequest req, WebResourceError rerr) {
         // Redirect to deprecated method, so you can use it in all SDK versions
         onReceivedError(view, rerr.getErrorCode(), rerr.getDescription().toString(), req.getUrl().toString());
-
     }
 
-//    /**
-//     * @param errorLayout 可以自定义一个用来展示的错误页面
-//     */
-//    protected void setErrorViewLayout(@LayoutRes int errorLayout) {
-//        this.errorLayout = errorLayout;
-//    }
-
+    /**
+     * @param errorLayout 可以自定义一个用来展示的错误页面
+     */
+    protected void setErrorViewLayout(@LayoutRes int errorLayout) {
+        this.errorLayout = errorLayout;
+    }
 
     /**
      * 显示自定义错误提示页面，用一个View覆盖在WebView
      */
 
-    public void showErrorPage(WebView webView) {
+    public void showErrorPage(final WebView webView) {
 
         if (errorViewStub == null) {
             errorViewStub = new ViewStub(webView.getContext());
-//            errorViewStub.setInflatedId(errorLayout);
             errorViewStub.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-//            errorViewStub.setLayoutInflater(LayoutInflater.from(webView.getContext()));
             errorViewStub.setLayoutResource(errorLayout);
             errorViewStub.setClickable(true);
             webView.addView(errorViewStub);
-
-//            View errorView = mErrorViewStub.inflate();
-//            errorView.findViewById(R.id.error_layout).setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    webView.reload();
-//                }
-//            });
+            View view = errorViewStub.inflate();
+            view.findViewById(reload).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    webView.reload();
+                }
+            });
         } else {
             errorViewStub.setVisibility(VISIBLE);
         }
         isError = true;
-
-//
-//        if (errorViewStub == null) {
-//            errorViewStub = new ViewStub(webView.getContext());
-//            errorViewStub.setLayoutResource(errorLayout);
-//            errorViewStub.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
-//            errorViewStub.setClickable(true);
-//            webView.addView(errorViewStub);
-//        } else {
-//            errorViewStub.setVisibility(VISIBLE);
-//        }
-
-
     }
-
 
     @Override
     public void onPageFinished(WebView view, String url) {
         super.onPageFinished(view, url);
+        if (!isError && errorViewStub != null)
+            errorViewStub.setVisibility(GONE);
     }
 
 }
